981ec858691611eb46e1de0d57e1cdb0b8c74a5f
sereyvathana <sereyvathana10@gmail.com>
2020-12-18 12:05:50 +0700

    add button and update readme

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Koompi Theme-manager
+
+## Introduction:
+Koompi Theme manager is an application that help user to easily change their theme.
+There are 6 themes suce as :
+- Koompi Dark
+- Koompi Light
+- Mac Dark 
+- Mac Light
+- Window Dark 
+- Window Light
+
 ## Screenshot :
 
 ![koompi theme](screenshots/theme.png?raw=true "home")
--- src/app.rs
+++ src/app.rs
@@ -1,16 +1,24 @@
-use iced::{ Sandbox, Row,  button, Button, Text, Column, Element, Length, Container, Svg};
+use button::State;
+use iced::{Button, Column, Container, Element, Length, Row, Sandbox, Svg, Text, button, futures::task::Spawn,Space};
 use iced_custom_widget::Grid;
+use crate::styles;
+
 use super::styles::CustomButton;
 
-#[derive(Debug,Default)]
+#[derive(Debug,Default)]        
 pub struct Theme{
     themes: Vec<(&'static str, &'static str, button::State)>,
     selected_theme: usize,
+    apply_btn:button::State,
+    cancel_btn:button::State,
+
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum AppMessage { 
     ItemClicked(usize),
+    ApplyBtn,
+    CancelBtn,
 }
 
 impl Sandbox for Theme{
@@ -28,7 +36,10 @@ impl Sandbox for Theme{
 
         Self {
             themes: items,
-            selected_theme: 0
+            selected_theme: 0,
+            apply_btn:State::new(),
+            cancel_btn: State::new(),
+
         }
     }
 
@@ -39,17 +50,25 @@ impl Sandbox for Theme{
     fn update(&mut self, message: Self::Message) {
         match message {
             AppMessage::ItemClicked(idx) => self.selected_theme = idx,
+            AppMessage::ApplyBtn =>{
+                print!("Apply Clicked")
+            },
+            AppMessage::CancelBtn =>{
+                print!("Cancel Clicked")
+            },
         }
     }
 
     fn view(&mut self) ->  iced::Element<'_, Self::Message> {
         let Theme {
             themes,
-            selected_theme
+            selected_theme,
+            apply_btn,
+            cancel_btn
         } = self;
 
-        let menu: Element<_> = themes.iter_mut().enumerate()
-            .fold(Grid::new().columns(3).spacing(50), |grid, (i, (path, title,state))| {
+        let themes: Element<_> = themes.iter_mut().enumerate()
+            .fold(Grid::new().columns(3).spacing(100), |grid, (i, (path, title,state))| {
                 let content = Container::new(
                     Column::new().spacing(14).align_items(iced::Align::Center)
                     .push(Svg::from_path(*path).width(Length::Units(75)).height(Length::Units(75)))
@@ -59,7 +78,23 @@ impl Sandbox for Theme{
                     Button::new(state, content).padding(15).on_press(AppMessage::ItemClicked(i)).style(if *selected_theme == i {CustomButton::SelectedCard} else {CustomButton::Card})
                 )
             }).into();
-
-        Container::new(menu).width(Length::Fill).height(Length::Fill).padding(20).center_x().center_y().into()
+        let themes_con = Container::new(themes).width(Length::Fill).center_x();
+        
+        let btn_group = Row::new().width(Length::Fill).spacing(20)
+        .push(Space::with_width(Length::Fill))
+        .push(
+            Button::new (
+                &mut self.apply_btn, 
+                Text::new("Add")).on_press(AppMessage::ApplyBtn),  
+        )
+        .push(
+            Button::new (
+                &mut self.cancel_btn, Text::new("Cancel")).on_press(AppMessage::CancelBtn),
+        );
+        
+        let main_col = Column::new().width(Length::Fill).spacing(15)
+            .push(themes_con)
+            .push(btn_group);
+        Container::new(main_col).width(Length::Fill).height(Length::Fill).padding(30).center_x().center_y().into()
     }
 }
